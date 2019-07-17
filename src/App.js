0ff20c4ae1ab56ee98871aece62041197fc8eff9
Andrew Bayles <baylesweb@gmail.com>
2019-07-17 16:18:36 -0500

    Add controllers and missing imports

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 //import logo from './logo.svg';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavTabs from "./components/NavTabs";
-import Search from "./components/pages/Search";
-import Saved from "./components/pages/Saved";
+import Search from "./components/Search";
+import Saved from "./components/Saved";
 import './App.css';
 
 function App() {
@@ -11,7 +11,9 @@ function App() {
 
     <Router>
       <div>
+
         <NavTabs />
+        
         <Route exact path="/" component={Search} />
         <Route exact path="/search" component={Search} />
         <Route exact path="/saved" component={Saved} />
